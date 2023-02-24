5c62f8eef832242c48c07b941414c2fb52874ec4
OlivierQuintal <oliquintal33@gmail.com>
2023-02-24 12:57:51 -0500

    test zero

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@ ESP32Servo360 servo;
 #define I2C_SCL 22
 uint8_t compass = 0;
 
+int vitesse = 1; // entre 0 et 255 
+
+
 
 int readCompass(void) {
   Wire.beginTransmission(0x60);
@@ -26,17 +29,38 @@ void setup() {
   Serial.begin(115200);
  
   servo.attach(4, 16); // Control pin (white), signal pin (yellow).
-  servo.setSpeed(140); // Set turns per minute (RPM), 140 max.
+  //servo.setSpeed(140); // Set turns per minute (RPM), 140 max.
 }
 
 void loop() {
+
+  /*
+    while (1) {
+      for (int i = 0; i < 20; i++) {
+        float duty = 140 * sin (i/20 * 2 * PI);
+        servo.spin(duty);
+        delay(1000);
+        Serial.println(duty);
+       }
+    }
+  */   
+
     // servo.spin(); // Turn at set speed clockwise.
     // delay(2000);
-    servo.spin(1); // Turn at 40 RPM anticlockwise.
+    if (vitesse != 0 )
+    {
+      //servo.attach(4, 16);
+      servo.spin(vitesse); // Turn at 40 RPM anticlockwise.
+    }else{
+      //servo.detach();
+
+    }
+    
     
     compass = readCompass();
     Serial.println(compass);
     delay(1000);
+ 
 
 
 }
