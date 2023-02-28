d6dc10fda2767e8c73dc2e1dbec870ba04772983
OlivierQuintal <oliquintal33@gmail.com>
2023-02-28 16:03:24 -0500

    bon code

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,32 @@ ESP32Servo360 servo;
 
 #define I2C_SDA 23
 #define I2C_SCL 22
-#define angle 100
-uint8_t compass = 0;
+#define angle 100     // angle de la boussole voulue (entre 0 et 255)
+
+
+float angleVoulue = 250 ; // angle entre 0 et 360 deg 
+
+uint8_t compass = 0;  // valeur de la boussole lu 
 
 int vitesse = 0; // entre 0 et 255 
+int attends = 0; // temps d'attente en ms
+const int moteur = 4; // pin du moteur
+
+float erreur = 0;   // erreur en degré entre la position voulue et la position actuelle
 
 
+void vitesses(float erreur){
+  
+  float sortieTemps = ((erreur * 0.00111) + 1.5);
+
+  digitalWrite(moteur, HIGH);
+  delay(sortieTemps);
+
+  float tempsBas = 20 - sortieTemps;
+  digitalWrite(moteur, LOW);
+  delay(tempsBas);
+  
+}
 
 int readCompass(void) {
   Wire.beginTransmission(0x60);
@@ -25,58 +45,72 @@ int readCompass(void) {
 }
 
 
+float positionEnDegre(int compass){
+  float angleEnDegre = compass * 360.0 / 255.0;
+ // Serial.println(angleEnDegre);
+  return angleEnDegre;
+}
+
+
+float rotation = 1.5 ;
 void setup() {
+  pinMode(moteur, OUTPUT);
   Wire.begin(I2C_SDA, I2C_SCL);
   Serial.begin(115200);
-  delay(500);
-  servo.attach(4, 16); // Control pin (white), signal pin (yellow).
   delay(1000);
-  //servo.setSpeed(140); // Set turns per minute (RPM), 140 max.
 }
 
 void loop() {
+  compass = readCompass();
+  erreur = angle - positionEnDegre(compass);
+  Serial.print("erreur : ");
+  Serial.println(erreur);
+
+  vitesses(erreur);
 
 
+  // for (int i = 0; i < 1000; i++)
+  // {
+  //   rotation = rotation - 0.01;
+  //   Serial.println(rotation);
+  //   servo.spin(rotation);
+  //   delay(100);
+  // }
+  // servo.spin(vitesse);
+    // compass = readCompass();
+    // float erreur = angle - compass;
+    // Serial.print("erreur : ");
+    // Serial.println(erreur);
 
+    // positionEnDegre(compass);
 
-    compass = readCompass();
+    // while(erreur > 3 || erreur < -3){
 
-    while(compass > angle +3 || compass < angle -3){
-
-      servo.attach(4, 16);
-      while (compass < angle -3)
-      {
-        vitesse = 10;
-        servo.spin(vitesse); // Turn at 40 RPM anticlockwise.
-        compass = readCompass();
-      }
-      while (compass > angle +3)
-      {
-        vitesse = -10;
-        servo.spin(vitesse); // Turn at 40 RPM anticlockwise.
-        compass = readCompass();
-      }
+    //   servo.attach(4, 16);
+    //   while (erreur < -3)
+    //   {
+    //     vitesse = 10;     // clockwise 
+    //     servo.spin(vitesse); 
+    //     compass = readCompass();
+    //     erreur = compass - angle;
+    //     //Serial.println(erreur);
+    //   }
+    //   while (erreur > 3)
+    //   {
+    //     vitesse = -1;     // conter clockwise
+    //     servo.spin(vitesse); 
+    //     compass = readCompass();
+    //     erreur = compass - angle;
+    //    // Serial.println(erreur);
+    //   }
       
-      compass = readCompass();
+    //   //compass = readCompass();
 
-
-
-    }
-
-    servo.detach();
-    //delay(1000);
-      
-
-    // servo.spin(); // Turn at set speed clockwise.
-    // delay(2000);
-    
-    
-    Serial.println(compass);
-    
-
-
-
+    // }
+    // servo.detach();  
+    // //Serial.println(compass);
 }
+
 
 
 
